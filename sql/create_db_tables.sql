b19ecc080ac4173a26369e672549d10cb752750b
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2023-09-14 23:03:01 -0400

    Fix create tables and bulk insert

--- sql/create_db_tables.sql
+++ sql/create_db_tables.sql
@@ -1,45 +1,74 @@
 USE master;
 
-CREATE DATABASE ams;
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'ams') 
+BEGIN
+    CREATE DATABASE ams 
+END;
 
 USE ams;
 
-CREATE TABLE country(
-    country_id CHAR(2) PRIMARY KEY,
-    country_name VARCHAR(100) NOT NULL,
-    region VARCHAR(20),
-    sub_region VARCHAR(50)
-);
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'country') 
+BEGIN
+    CREATE TABLE country(
+        country_id CHAR(2) PRIMARY KEY,
+        country_name VARCHAR(100) NOT NULL,
+        region VARCHAR(20),
+        sub_region VARCHAR(50)
+    ) 
+END;
 
-CREATE TABLE shipment(
-    shipment_id BIGINT PRIMARY KEY,
-    carrier_code VARCHAR(4),
-    vessel_country_code CHAR(2) FOREIGN KEY REFERENCES country(country_id),
-    vessel_name VARCHAR(50),
-    estimated_arrival_date DATE,
-    actual_arrival_date DATE
-);
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'vessel') 
+BEGIN
+    CREATE TABLE vessel(
+        vessel_id INT PRIMARY KEY,
+        vessel_name VARCHAR(50),
+        vessel_country_id CHAR(2) FOREIGN KEY REFERENCES country(country_id),
+        carrier_code VARCHAR(4)
+    ) 
+END;
 
-CREATE TABLE shipper(
-    shipper_id INT PRIMARY KEY,
-    shipper_name VARCHAR(75),
-    shipper_country_code CHAR(2) FOREIGN KEY REFERENCES country(country_id)
-);
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'shipment') 
+BEGIN
+    CREATE TABLE shipment(
+        shipment_id BIGINT PRIMARY KEY,
+        vessel_id INT,
+        estimated_arrival_date DATE,
+        actual_arrival_date DATE
+    ) 
+END;
 
-CREATE TABLE consignee(
-    consignee_id INT PRIMARY KEY,
-    consignee_name VARCHAR(75),
-    consignee_country_code CHAR(2) FOREIGN KEY REFERENCES country(country_id)
-);
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'shipper') 
+BEGIN
+    CREATE TABLE shipper(
+        shipper_id INT PRIMARY KEY,
+        shipper_name VARCHAR(75),
+        shipper_country_code CHAR(2) FOREIGN KEY REFERENCES country(country_id)
+    ) 
+END;
 
-CREATE TABLE shipper_shipment(
-    shipper_shipment_id INT PRIMARY KEY,
-    shipment_id BIGINT FOREIGN KEY REFERENCES shipment(shipment_id),
-    shipper_id INT FOREIGN KEY REFERENCES shipper(shipper_id)
-);
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'consignee') 
+BEGIN
+    CREATE TABLE consignee(
+        consignee_id INT PRIMARY KEY,
+        consignee_name VARCHAR(75),
+        consignee_country_code CHAR(2) FOREIGN KEY REFERENCES country(country_id)
+    ) 
+END;
 
-CREATE TABLE consignee_shipment(
-    consignee_shipment_id INT PRIMARY KEY,
-    shipment_id BIGINT FOREIGN KEY REFERENCES shipment(shipment_id),
-    consignee_id INT FOREIGN KEY REFERENCES consignee(consignee_id)
-);
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'shipper_shipment') 
+BEGIN 
+    CREATE TABLE shipper_shipment(
+        shipper_shipment_id INT PRIMARY KEY,
+        shipment_id BIGINT FOREIGN KEY REFERENCES shipment(shipment_id),
+        shipper_id INT FOREIGN KEY REFERENCES shipper(shipper_id)
+    ) 
+END;
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'consignee_shipment') 
+BEGIN
+    CREATE TABLE consignee_shipment(
+        consignee_shipment_id INT PRIMARY KEY,
+        shipment_id BIGINT FOREIGN KEY REFERENCES shipment(shipment_id),
+        consignee_id INT FOREIGN KEY REFERENCES consignee(consignee_id)
+    ) 
+END;
--- sql/load_db_tables.sql
+++ sql/load_db_tables.sql
@@ -3,6 +3,9 @@ USE ams;
 BULK INSERT dbo.country FROM '/home/data/countries.csv' WITH (
     FORMAT='CSV', ROWTERMINATOR='0x0a', FIRSTROW=2
 );
+BULK INSERT dbo.vessel FROM '/home/data/vessel.csv' WITH (
+    FORMAT='CSV', ROWTERMINATOR='0x0a', FIRSTROW=2
+);
 BULK INSERT dbo.shipment FROM '/home/data/shipment.csv' WITH (
     FORMAT='CSV', ROWTERMINATOR='0x0a', FIRSTROW=2
 );
